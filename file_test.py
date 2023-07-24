f1a45a03c24dc89fcc6ba91d92db7774f074a936
Mamadou <mamadou.ciss@outlook.com>
2023-07-24 14:03:34 +0000

    fix code

--- file_test.py
+++ file_test.py
@@ -1,5 +1,3 @@
-import pytest
-
 def test_calc_addition():
     # Fonction test du résultat de 2 + 4
     output = 2 + 4
@@ -18,5 +16,4 @@ def test_calc_multiply():
 def test_coucou():
     # Fonction test si le résultat renvoie 'hello'
     output = 'hello'
-    assert output == 'hello'
-    
+    assert output == 'hello'  
